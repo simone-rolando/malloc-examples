b2c823b27aa037e873248b0e7f8e243c2fed66cd
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-03 12:31:29 +0100

    cambiata costante sz, aggiunto ciao

--- example_native.c
+++ example_native.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int sz = 20;
+
 void print_array(int a[], int sz);
 
 int main(int argc, char** argv)
 {
-    int sz = 20;
-
     int* dyn = (int*)malloc(sizeof(int));
     int* dyn_arr = (int*)malloc(sizeof(int) * sz);
+
+    int pippo[sz];
+    printf("Ciao\n");
 
     *dyn = 25;
     printf("%d\n", *dyn);
